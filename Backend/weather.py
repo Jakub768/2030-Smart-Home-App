8b157f06047d7d8f17ccacb6ffce85220b6dc428
Jakub <jb2076@hw.ac.uk>
2025-02-03 13:13:22 +0000

    implement getting temperature

--- Backend/weather.py
+++ Backend/weather.py
@@ -2,8 +2,17 @@ import requests
 
 def getWeatherData(cityName, APIKey):
     URL = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}"
-    return requests.get(URL)
+    req = requests.get(URL)
 
-curWWeather = getWeatherData("Edinburgh", "05f9f314f71d9d69f947eeffac9a4a1d")
+    return req.json()
 
-print(curWWeather.json())
+def getTemperature(weatherData):
+    #note: json returns temperature in kelvin, convert into celsius.
+    KELVIN_CONSTANT = 273.15
+    temperature = weatherData['main']['temp']
+
+    return temperature - KELVIN_CONSTANT
+
+curWeather = getWeatherData("Edinburgh", "05f9f314f71d9d69f947eeffac9a4a1d")
+
+print(getTemperature(curWeather))
