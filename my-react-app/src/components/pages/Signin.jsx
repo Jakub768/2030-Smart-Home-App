4a7f3ef2a21888084f4dc2c8f4bbc44a44fd55a8
Nathan Johnston <nj2006@hw.ac.uk>
2025-02-03 13:28:10 +0000

    added sign in page

--- my-react-app/src/components/pages/Signin.jsx
+++ my-react-app/src/components/pages/Signin.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const Signin = () => {
-  return <h1>Signin</h1>;
+  return (
+    <div className='container'>
+     <div className='header'>
+      <div className="signupHeader">Sign Up</div>
+     </div>
+     <div className='inputs'>
+      <div className='input'>
+       <input type="text" placeholder="Username"></input>
+      </div>
+      <div className='input'>
+       <input type="password" placeholder="Password"></input>
+      </div>
+     </div>
+    <div className='submitContainer'>
+      <div className="submit">Sign Up</div>
+      <div className="submit">Login</div>
+    </div>
+    </div>
+  );
 };
