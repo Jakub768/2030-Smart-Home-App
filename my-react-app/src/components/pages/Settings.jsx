b0c928f1aff95622feb5ef5bdeaa2ce1dc6abe8f
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2025-02-21 16:34:18 +0000

    Settings - Button Decoration

--- my-react-app/src/components/pages/Settings.jsx
+++ my-react-app/src/components/pages/Settings.jsx
@@ -10,43 +10,42 @@ export const Settings = () => {
         <div className="settings-columns">
           {/* Left Side */}
           <div className="settings-left">
-            <button>My Profile</button>
-            <button>Wi-Fi</button>
-            <button>Bluetooth</button>
+          <button className="no-gap1">My Profile</button>
+          <button className="no-gap">Wi-Fi</button>
+          <button className="no-gap2">Bluetooth</button>
+
 
             <h2>Permissions</h2>
-            <ul>
-              <li>Location <input type="checkbox" /></li>
-              <li>Camera <input type="checkbox" /></li>
-              <li>Microphone <input type="checkbox" /></li>
-            </ul>
-
-            <button>Help</button>
+            <button className="no-gap1">Location</button>
+            <button className="no-gap">Camera</button>
+            <button className="no-gap">Microphone</button>
+            <button className="no-gap2">PH</button>
             
-            <div className="settings-actions">
+            <div className="individual-button help-button">
+                <button>Help</button>
+            </div>
+
+
+            <div className="individual-button sign-out-button">
               <button>Sign out</button>
             </div>
           </div>
 
           {/* Right Side */}
           <div className="settings-right">
-          <button>About</button>
-          <button>Software Update</button>
+            <button className="no-gap1">About</button>
+            <button className="no-gap2">Software Update</button>
 
             <h2>Display</h2>
-            <ul>
-              <li><button>Background</button></li>
-              <li><button>Theme</button></li>
-            </ul>
+            <button className="no-gap1">Background</button>
+            <button className="no-gap2">Theme</button>
 
             <h2>Other</h2>
-            <ul>
-              <li><button>Notifications</button></li>
-              <li><button>Sounds</button></li>
-              <li><button>Accessibility</button></li>
-            </ul>
+            <button className="no-gap1">Notifications</button>
+            <button className="no-gap">Sounds</button>
+            <button className="no-gap2">Accessibility</button>
             
-            <div className="settings-actions">
+            <div className="individual-button">
               <button>Change user</button>
             </div>
           </div>
