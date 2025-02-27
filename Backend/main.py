81a1dbc5179518368c807d7927f3dee8a92c84db
Jack <whitefordjack03@gmail.com>
2025-02-27 12:52:51 +0000

    main with todo for backend

---
+++ Backend/main.py
@@ -0,0 +1,64 @@
+#KEY
+#? -should
+#   -must/important
+#thing? - unsure if added
+
+#call/run weather.py every 15 minutes
+
+#import/export data from weather
+
+#import/export data from/to SQL
+
+#HTTP stuff
+
+#make up the data
+
+
+##SQL (admin)
+#user access add
+#user access delete 
+#user access edit
+
+#send email to user on signup
+
+#user login (username & password)
+
+#logout (/timeout?)
+
+#admin mode - allow edit
+
+#GDPR delete data
+
+#add device
+#remove device
+
+#?categorise device
+
+#turn on/off 'device' in app
+#?schedule on/off of devices
+
+
+#data (collect & show)
+#energy consumption
+#energy generation
+#robot usage
+#status
+#device consumption
+
+#sort by energy consumption
+
+#stat breakdown ?
+
+#stat compare last 2 weeks
+
+#connect to internet
+
+#connect to devices
+
+#?reboot
+
+#?error log
+
+#permissions (owner/admin/user/guest)
+#owner set up house
+
