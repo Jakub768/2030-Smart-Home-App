099a409b09d364678f33b07c92d88a7b147af313
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2025-03-04 12:51:04 +0000

    SQL

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,7 @@
 
 #import/export data from weather
 
-#import/export data from/to SQL
+#import/export data from/to SQL Check
 
 #HTTP stuff
 
@@ -61,24 +61,3 @@
 
 #permissions (owner/admin/user/guest)
 #owner set up house
-
-import mysql.connector;
-
-mydb = mysql.connector.connect(
-  host="virtual-butler-minchengpiao03152004-8434.g.aivencloud.com",
-  port=19919,
-  user="avnadmin",
-  password="AVNS_7DhSsm_1tMr-6iMrMjc",
-  database="virtual_butler"
-)
-
-cursor = mydb.cursor()
-
-cursor.execute("SELECT * FROM Users")
-
-results = cursor.fetchall()
-for row in results:
-    print(row)
-
-cursor.close()
-mydb.close()
