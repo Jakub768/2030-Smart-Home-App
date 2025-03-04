b5dea7fa93877f0805b63917c0f17af20972b41d
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2025-03-04 12:46:49 +0000

    SQL

--- Backend/main.py
+++ Backend/main.py
@@ -62,3 +62,23 @@
 #permissions (owner/admin/user/guest)
 #owner set up house
 
+import mysql.connector;
+
+mydb = mysql.connector.connect(
+  host="virtual-butler-minchengpiao03152004-8434.g.aivencloud.com",
+  port=19919,
+  user="avnadmin",
+  password="AVNS_7DhSsm_1tMr-6iMrMjc",
+  database="virtual_butler"
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("SELECT * FROM Users")
+
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+cursor.close()
+mydb.close()
