d8b908130a3d8ef6e677bf4b69872556483301ab
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2025-03-05 08:57:30 +0000

    Add check for commit

--- Backend/execute_DB.py
+++ Backend/execute_DB.py
@@ -11,7 +11,14 @@ def execute_SQL(query, params=None):
             database="virtual_butler"
         ) as mydb, mydb.cursor() as cursor:
             cursor.execute(query, params) if params else cursor.execute(query)
-            results = cursor.fetchall() if query.strip().lower().startswith("select") else (mydb.commit() or cursor.rowcount)
+            
+            if query.strip().lower().startswith("select"):
+                results = cursor.fetchall()
+            else:
+                mydb.commit()
+                print("Commit successful!")
+                results = cursor.rowcount
+
         return results
     except Error as e:
         return f"Error: {e}"
