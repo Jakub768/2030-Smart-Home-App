33bd780ba50290331daffe7803fa31187a51eb8f
testkubby <80340234+Jakub768@users.noreply.github.com>
2025-03-05 13:26:18 +0000

    add_device function

--- Backend/device_manager.py
+++ Backend/device_manager.py
@@ -1,6 +1,25 @@
+import execute_DB
+
 def add_device(Name, Type, roomID, userID, Consumption, energyGen, status):
-    pass
+
+    data = (
+        Name,
+        Type,
+        roomID,
+        userID,
+        Consumption,
+        energyGen,
+        status
+    )
+
+    print("lol")
+    execute_DB.execute_SQL(
+        "INSERT INTO Devices (deviceName, deviceType, roomID, userID, energyConsumption, energyGeneration, status) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        data
+    )
+    
 
 def remove_device(deviceID):
     pass
 
+add_device("haha", "lol", 1, 1, 20.0, 15.0, 'active')
