e96d805c0f2ccb566d95218061ad1a0e6c8d9b11
MCP2001Hw <145106701+MCP2001Hw@users.noreply.github.com>
2025-03-05 09:55:50 +0000

    Added getWeather API

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
-import weather_auto_update
+from flask import Flask, jsonify, request
+import execute_DB
 
 app = Flask(__name__)
+
+@app.route('/api/weather', methods=['GET'])
+def get_weathers():
+    get_latest_weather_data = """
+    SELECT temperature, humidity, windSpeed, weatherType
+    FROM Weather
+    WHERE houseID = %s
+    ORDER BY timestamp DESC
+    LIMIT 1;
+    """
+    weather_data = execute_DB.execute_SQL(get_latest_weather_data, (1,))
+    return jsonify(weather_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
