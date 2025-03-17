1f96f87aa185e969dcf1f922c448c4305eab4217
SeanRJC <91936749+SeanRJC@users.noreply.github.com>
2025-03-17 17:29:35 +0000

    users page working

--- my-react-app/src/components/pages/Settings.jsx
+++ my-react-app/src/components/pages/Settings.jsx
@@ -77,8 +77,8 @@ export const Settings = () => {
           </div>
 
           <div className="settings-right">
-            <button className="no-gap1" onClick={() => navigate("/about")}>About<span className="arrow">&gt;</span></button>
-            <button className="no-gap1" onClick={() => navigate("/update")}>Software Update<span className="arrow">&gt;</span></button>
+            <button className="no-gap1">About<span className="arrow">&gt;</span></button>
+            <button className="no-gap2">Software Update<span className="arrow">&gt;</span></button>
 
             <h2>Accessibility</h2>
             <button className="no-gap1">Background<span className="arrow">&gt;</span></button>
